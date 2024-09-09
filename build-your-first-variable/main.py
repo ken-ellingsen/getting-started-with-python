bc9515a34115fcf00fb486957b5aa07313f5827b
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-09 16:36:22 -0500

    Completed lesson (9/9/24)

--- build-your-first-variable/main.py
+++ build-your-first-variable/main.py
@@ -1,1 +1,5 @@
-skillz = "Howdy, I’m busy crushing skillz "
+skillz = "Howdy, I'm busy crushing skillz "
+
+# Use the repetition operator to multiply the `skillz` variable 3 times, 
+# then add a plus sign after the number 3 to concatenate the `skillz` variable with a string that reads "And I'm killing it!"
+print(skillz * 3 + "And I'm killing it!")
