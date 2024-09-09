fde858e697d4a01fd48539554c3b8290e0ec43de
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-09 16:41:31 -0500

    Completed lesson (9/9/24)

---
+++ math-can-be-fun-with-python/main.py
@@ -0,0 +1,12 @@
+# In a print function, divide 60 by 23
+print(60/23)
+
+# In a second print() function, write the arithmetic operators to multiply 20 times 3 plus 18
+print(20*3+18)
+
+#  Assign a variable called pay equal to 3800 and another variable named expenses equal to 2200
+pay = 3800
+expenses = 2200
+
+#  Write another print() function. Inside the function subtract expenses from pay
+print(pay-expenses)
