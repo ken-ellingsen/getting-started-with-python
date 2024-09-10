423f9a449ca7b12a270366ace66a0c77097517fe
ken_ellingsen <ken.ellingsen@gmail.com>
2024-09-10 00:18:13 -0500

    Added if/elif/else statements to finish app. Completed lesson (9/10/24)

--- build-a-birthday-reminder-app/main.py
+++ build-a-birthday-reminder-app/main.py
@@ -1,1 +1,10 @@
 user_input = input("Which birthday are you looking for? ")
+
+if user_input == "Meg":
+    print("11/15/98")
+elif user_input == "Pixie":
+    print("1/13/08")
+elif user_input == "Ken":
+    print("2/16/96")
+else:
+    print("Can't find a birthday for this person...")
